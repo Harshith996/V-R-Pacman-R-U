222af8208e5184de3538ceccef84741732626d05
Harshith996 <50741631+Harshith996@users.noreply.github.com>
2020-10-02 12:37:17 +0530

    Initial commit

---
+++ Assets/ColorChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+    public Material[] mat;
+    public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = mat[0];
+	}
+	
+	
+
+}
